daddb842145345b26efebe386e1116b7d91d929d
kumard3 <kumardeepanshu157200@gmail.com>
2022-03-08 11:07:34 +0530

    Document added

---
+++ src/pages/_document.tsx
@@ -0,0 +1,23 @@
+import Document, { DocumentContext, Head, Html, Main, NextScript } from 'next/document'
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const initialProps = await Document.getInitialProps(ctx)
+
+    return initialProps
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head />
+        <body className="bg-black w-full min-h-screen text-white ">
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
+
+export default MyDocument
