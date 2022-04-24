7da56b0165d5ba3b8be46993b751d9aed567f5f7
kumard3 <kumardeepanshu157200@gmail.com>
2022-04-24 10:33:05 +0530

    updated

--- src/pages/templates/portfolio2/[username].tsx
+++ src/pages/templates/portfolio2/[username].tsx
@@ -2,7 +2,7 @@ import { GetServerSideProps } from 'next'
 import React, { useEffect, useState } from 'react'
 
 import BottomNav from '../../../components/BottomNav'
-import Hero from '../../../components/template-2/components/Hero'
+
 
 import { useLocalStorage } from '../../../helper/useLocalStorage'
 import VercelButton2 from '../../../components/VercelButton/VercelButton2'
@@ -27,7 +27,34 @@ export default function Portfolio2({ name, bio, githubRepoData }: any) {
   return (
     <div>
       <VercelButton2 />
-      <Hero name={name} bio={bio} />
+      <div className="relative w-full  container px-4 mx-auto flex flex-col justify-center items-center h-screen -mt-20 ">
+        <div className="bg-[#60A5FA] h-[120px] top-36 right-20 absolute w-[120px] rounded-full blur-[90px] filter "></div>
+        <div className="bg-[#60A5FA] h-[120px] top-[200px] right-[200px] absolute w-[120px] rounded-full blur-[90px] filter "></div>
+        <div className="flex flex-col items-start relative ">
+          <div className="flex flex-col items-start SpaceGroteskBold text-[28px] md:text-[50px] lg:text-[64px] ">
+            <h1>Hello!👋🏻</h1>
+            <h1>My name is {name}</h1>
+            {/* <h1>
+            I'm a Front End <span className="text">Developer</span>
+          </h1> */}
+          </div>
+          <div className="bg-[#60A5FA] h-[120px] top-[200px] left-[5px] absolute w-[120px] rounded-full blur-[90px] filter "></div>
+
+          <div className="SpaceGroteskRegular text-[16px] md:text-[20px] lg:text-[24px] py-5">
+            <p>{bio}</p>
+          </div>
+          <>
+            <div className="relative block group  my-7 ml-4 cursor-pointer">
+              <span className="absolute inset-0 border-2  border-[#188AEC] rounded-lg"></span>
+              <div className="transition bg-[#188AEC] text-white rounded-lg group-hover:-translate-x-0  group-hover:-translate-y-0 -translate-x-3 translate-y-2">
+                <div className="py-3 px-10 ">
+                  <p className="mt-1 text-xl">Say Hello</p>
+                </div>
+              </div>
+            </div>
+          </>
+        </div>
+      </div>
       <BottomNav />
     </div>
   )
