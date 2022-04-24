b457dd5ed5ec57703a0ee9dc1548a060fb0afbca
kumard3 <kumardeepanshu157200@gmail.com>
2022-04-24 13:44:25 +0530

    seo added

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 /* eslint-disable @next/next/no-img-element */
 /* eslint-disable @next/next/link-passhref */
+import Head from 'next/head'
 import Image from 'next/image'
 import * as React from 'react'
 import { useForm, SubmitHandler } from 'react-hook-form'
@@ -15,7 +16,7 @@ export default function Home() {
   const { register, handleSubmit } = useForm<FormValues>({
     mode: 'onChange',
   })
-  const [name, setName] = useLocalStorage<string>('name','')
+  const [name, setName] = useLocalStorage<string>('name', '')
 
   const onSubmit: SubmitHandler<FormValues> = (data) => {
     if (!data.username) return
@@ -35,53 +36,113 @@ export default function Home() {
     GtihubReadMe()
   }, [])
   return (
-    <div className="bg-black  text-white  flex justify-center flex-col items-center w-full min-h-screen ">
-      <div className="bg-slate-100 rounded-[5px] px-[20px] py-[10px] my-10">
-        <img src="/logo.svg" alt="DevFolio logo" />
-      </div>
+    <>
+      <Head>
+        <title>DevFolio</title>
+        <meta
+          name="description"
+          content="Devfolio is an open source application which allows developers to create a portfolio in less than 2 minutes with just their username."
+        />
+        <meta
+          name="viewport"
+          content="width=device-width,minimum-scale=1,initial-scale=1"
+        />
+        <meta property="og:type" content="website" />
+        <meta name="og:title" property="og:title" content="DevFolio" />
+        <meta
+          name="og:description"
+          property="og:description"
+          content="Devfolio is an open source application which allows developers to create a portfolio in less than 2 minutes with just their username."
+        />
+        <meta
+          property="og:site_name"
+          content="DevFolio - Create a portfolio in less than 2 minutes"
+        />
 
-      <h1 className="text-7xl font-bold max-w-5xl text-center my-10 ">
-        Just type your username and wait for the magic
-      </h1>
-      <form onSubmit={handleSubmit(onSubmit)} className=" flex flex-col ">
-        <div className="flex flex-col sm:flex-row justify-center items-center my-5">
-          <input
-            {...register('username')}
-            placeholder="GitHub Username"
-            className="text-black text-xl font-mono text-center my-5 rounded-xl  sm:w-2/3 px-3 py-[.7em] sm:rounded-l-xl sm:rounded-r-none"
-          />
-          <button
-            type="submit"
-            className="bg-[#141416]/80  hover:bg-[#141416] py-[1em] sm:rounded-r-xl sm:rounded-l-none w-[10rem] rounded-xl "
-          >
-            {' '}
-            submit
-          </button>
+        <meta
+          name="twitter:card"
+          content="DevFolio - Create a portfolio in less than 2 minutes"
+        />
+        <meta name="twitter:title" content="DevFolio" />
+        <meta
+          name="twitter:description"
+          content="Devfolio is an open source application which allows developers to create a portfolio in less than 2 minutes with just their username."
+        />
+        <meta name="twitter:site" content="@propernoun" />
+        <meta name="twitter:creator" content="@propernounco" />
+
+        <meta
+          property="og:image"
+          content="https://devfolio.kumard3.me/ogImage.png"
+        />
+        <meta
+          name="twitter:image"
+          content="https://devfolio.kumard3.me/ogImage.png"
+        />
+
+        {/* Open Graph / Facebook */}
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://devfolio.kumard3.me" />
+        <meta property="og:title" content="DevFolio" />
+        <meta
+          property="og:description"
+          content="Devfolio is an open source application which allows developers to create a portfolio in less than 2 minutes with just their username."
+        />
+        <meta
+          property="og:image"
+          content="https://devfolio.kumard3.me/ogImage.png"
+        />
+        <meta property="og:site_name" content="DevFolio" />
+        {/* Twitter */}
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content="https://devfolio.kumard3.me" />
+        <meta property="twitter:title" content="DevFolio" />
+        <meta
+          property="twitter:description"
+          content="Devfolio is an open source application which allows developers to create a portfolio in less than 2 minutes with just their username."
+        />
+        <meta
+          property="twitter:image"
+          content="https://devfolio.kumard3.me/ogImage.png"
+        />
+        <meta name="robots" content="Index" />
+        <link rel="manifest" href="/manifest.json" />
+      </Head>
+      <div className="bg-black  text-white  flex justify-center flex-col items-center w-full min-h-screen ">
+        <div className="bg-slate-100 rounded-[5px] px-[20px] py-[10px] my-10">
+          <img src="/logo.svg" alt="DevFolio logo" />
         </div>
-      </form>
-      <h1 className="font-mono flex flex-col items-center">
-        Available requests
-        <span>60/{getRateRemaining}</span>
-      </h1>
-      <footer className="font-mono text-xl py-10 relative">
-        Created by{' '}
-        <a href="https://github.com/kumard3" target="_blank" rel="noreferrer">
-          Kumar Deepanshu
-        </a>
-      </footer>
-    </div>
+
+        <h1 className="text-7xl font-bold max-w-5xl text-center my-10 ">
+          Just type your username and wait for the magic
+        </h1>
+        <form onSubmit={handleSubmit(onSubmit)} className=" flex flex-col ">
+          <div className="flex flex-col sm:flex-row justify-center items-center my-5">
+            <input
+              {...register('username')}
+              placeholder="GitHub Username"
+              className="text-black text-xl font-mono text-center my-5 rounded-xl  sm:w-2/3 px-3 py-[.7em] sm:rounded-l-xl sm:rounded-r-none"
+            />
+            <button
+              type="submit"
+              className="bg-[#141416]/80  hover:bg-[#141416] py-[1em] sm:rounded-r-xl sm:rounded-l-none w-[10rem] rounded-xl "
+            >
+              {' '}
+              submit
+            </button>
+          </div>
+        </form>
+        <h1 className="font-mono flex flex-col items-center">
+          Available requests
+          <span>60/{getRateRemaining}</span>
+        </h1>
+        <footer className="font-mono text-xl py-10 relative">
+          Created by{' '}
+          <a href="https://github.com/kumard3" target="_blank" rel="noreferrer">
+            Kumar Deepanshu
+          </a>
+        </footer>
+      </div>
+    </>
   )
 }
-
-//https://raw.githubusercontent.com/kumard3/kumard3/main/README.mdexport default function App() {
-//   const { register, handleSubmit, errors } = useForm();
-//   const onSubmit = (data) => console.log(data);
-
-//   return ( #141416 #141416 #23262F #141416
-//     <form onSubmit={handleSubmit(onSubmit)}>
-//       <input {...register("username")} />
-
-//       <input type="submit" />
-//     </form>
-//   );
-// }
--- src/pages/templates/[username].tsx
+++ src/pages/templates/[username].tsx
@@ -1,6 +1,7 @@
 /* eslint-disable @next/next/no-img-element */
 import React, { useEffect, useState } from 'react'
 import { useRouter } from 'next/router'
+import Head from 'next/head'
 
 export default function TemplateName() {
   const [items, setItems] = useState('')
@@ -39,6 +40,77 @@ export default function TemplateName() {
 
   return (
     <>
+      <Head>
+        <title>DevFolio-Template</title>
+        <meta
+          name="description"
+          content="Devfolio is an open source application which allows developers to create a portfolio in less than 2 minutes with just their username."
+        />
+        <meta
+          name="viewport"
+          content="width=device-width,minimum-scale=1,initial-scale=1"
+        />
+        <meta property="og:type" content="website" />
+        <meta name="og:title" property="og:title" content="DevFolio" />
+        <meta
+          name="og:description"
+          property="og:description"
+          content="Devfolio is an open source application which allows developers to create a portfolio in less than 2 minutes with just their username."
+        />
+        <meta
+          property="og:site_name"
+          content="DevFolio - Create a portfolio in less than 2 minutes"
+        />
+
+        <meta
+          name="twitter:card"
+          content="DevFolio - Create a portfolio in less than 2 minutes"
+        />
+        <meta name="twitter:title" content="DevFolio" />
+        <meta
+          name="twitter:description"
+          content="Devfolio is an open source application which allows developers to create a portfolio in less than 2 minutes with just their username."
+        />
+        <meta name="twitter:site" content="@propernoun" />
+        <meta name="twitter:creator" content="@propernounco" />
+
+        <meta
+          property="og:image"
+          content="https://devfolio.kumard3.me/ogImageTemplate.png"
+        />
+        <meta
+          name="twitter:image"
+          content="https://devfolio.kumard3.me/ogImageTemplate.png"
+        />
+
+        {/* Open Graph / Facebook */}
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://devfolio.kumard3.me" />
+        <meta property="og:title" content="DevFolio" />
+        <meta
+          property="og:description"
+          content="Devfolio is an open source application which allows developers to create a portfolio in less than 2 minutes with just their username."
+        />
+        <meta
+          property="og:image"
+          content="https://devfolio.kumard3.me/ogImageTemplate.png"
+        />
+        <meta property="og:site_name" content="DevFolio" />
+        {/* Twitter */}
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content="https://devfolio.kumard3.me" />
+        <meta property="twitter:title" content="DevFolio" />
+        <meta
+          property="twitter:description"
+          content="Devfolio is an open source application which allows developers to create a portfolio in less than 2 minutes with just their username."
+        />
+        <meta
+          property="twitter:image"
+          content="https://devfolio.kumard3.me/ogImageTemplate.png"
+        />
+        <meta name="robots" content="Index" />
+        <link rel="manifest" href="/manifest.json" />
+      </Head>
       <h1 className="text-5xl text-center py-10 font-mono font-bold">
         Select a template
       </h1>
